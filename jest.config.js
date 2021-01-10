aad794a7fb1bb8df5ac290d1dd9d5bbb050a24ac
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-10 15:21:46 -0300

    Initial commit from Create Next App

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/mock.ts',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
+}
