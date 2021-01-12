38bcb87f4e75d8b74c73d950f45534a9819c5884
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-12 17:30:56 -0300

    Creating enum with config of theme.ts

---
+++ src/styles/enum/default.ts
@@ -0,0 +1,62 @@
+export enum Grid {
+  container = '130rem',
+  gutter = '3.2rem',
+}
+
+export enum Radius {
+  radius = '0.4rem',
+}
+
+export enum Family {
+  family = "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+}
+
+export enum Weight {
+  light = '300',
+  regular = '400',
+  medium = '500',
+  bold = '700',
+}
+
+export enum Sizes {
+  xxsmall = '1.8rem',
+  xsmall = '2.0rem',
+  small = '2.8rem',
+  medium = '3.2rem',
+  large = '3.4rem',
+  xlarge = '3.6rem',
+  xxlarge = '4.8rem',
+}
+
+export enum Colors {
+  primary = '#EE4248',
+  littleBlack = '#263238',
+  mediumBlack = '#1F2E35',
+  black = '#30444E',
+  darker = '#1F2E35',
+  white = '#FFFFFF',
+}
+
+export enum Spacings {
+  xxsmall = '0.6rem',
+  xsmall = '1rem',
+  small = '1.6rem',
+  medium = '2.4rem',
+  large = '2.9rem',
+  xlarge = '3.2rem',
+  xxlarge = '3.5rem',
+  huge = '5rem',
+}
+
+export enum Layers {
+  base = '10',
+  menu = '20',
+  overlay = '30',
+  modal = '40',
+  alwaysOnTop = '50',
+}
+
+export enum Transition {
+  default = '0.3s ease-in-out',
+  fast = '0.1s ease-in-out',
+}
