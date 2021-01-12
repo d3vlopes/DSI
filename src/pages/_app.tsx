9262c8772707ed25db36a9db2a3abee0b9d9e9cb
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-12 19:51:24 -0300

    Config the _app.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <title>DSI - Home</title>
+        <link rel="shortcut icon" href="/img/icon.png" />
+        <link rel="apple-touch-icon" href="/img/icon.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
