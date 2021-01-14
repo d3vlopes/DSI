de838a09de969f863b52de3b9e7addd924338ef6
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-14 13:47:38 -0300

    UPDATE inline snapshot of Grid component

--- src/components/Grid/test.tsx
+++ src/components/Grid/test.tsx
@@ -9,9 +9,9 @@ describe('<Grid />', () => {
     expect(container.firstChild).toMatchInlineSnapshot(`
       .c0 {
         display: grid;
-        grid-template-columns: repeat(auto-fill,minmax(25rem,1fr));
-        grid-gap: 3.2rem;
-        margin: 3.2rem 0;
+        grid-template-columns: repeat(auto-fill,minmax(23rem,1fr));
+        grid-gap: 2.4rem;
+        margin: 2.4rem 0;
       }
 
       <div
