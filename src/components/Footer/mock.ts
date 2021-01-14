7ee227729b945a6b1c16a5e2139ef9caecddf7ee
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-14 20:17:36 -0300

    Add mock for Footer component

---
+++ src/components/Footer/mock.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: '/img/platform/microsoft.png',
+    alt: 'Microsoft Silverlight',
+  },
+  {
+    src: '/img/platform/net.png',
+    alt: 'Microsoft .Net Framework',
+  },
+  {
+    src: '/img/platform/java.png',
+    alt: 'Java',
+  },
+  {
+    src: '/img/platform/ios.png',
+    alt: 'IOS',
+  },
+  {
+    src: '/img/platform/android.png',
+    alt: 'Android',
+  },
+  {
+    src: '/img/platform/black-berry.png',
+    alt: 'Black Berry',
+  },
+]
