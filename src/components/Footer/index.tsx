6ae0821dfccbdd8606624126738a260ade09d3e2
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-14 20:17:58 -0300

    Creating structure of Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,48 @@
+import mock from './mock'
+
 import * as S from './styles'
 
-const Footer = () => (
-  <S.Wrapper>
-    <h1>Footer</h1>
-  </S.Wrapper>
-)
+const Footer = () => {
+  const date = new Date()
+
+  return (
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.png"
+        alt="Letters DSI and words DWIDASA, SAMSARA, INDONESIA"
+      />
+      <S.Content>
+        <S.Address>
+          <h4>PT Dwidasa Samsara Indonesia</h4>
+          <div>
+            <span>Ruko Jalur Sutera 29A No. 53</span>
+            <span>Alam Sutera Serpong, Tangerang 15323</span>
+          </div>
+        </S.Address>
+
+        <S.Contact>
+          <h3>Contact</h3>
+          <div>
+            <span>Phone : +62.21.5314.1135</span>
+            <span>Fax : +62.21.5314.1135</span>
+            <span>Email : community@dwidasa.com</span>
+          </div>
+        </S.Contact>
+
+        <S.Platform>
+          {mock.map((item, index) => (
+            <img
+              key={`platform-${index}`}
+              id={`platform-${index}`}
+              src={item.src}
+              alt={`icon of ${item.alt}`}
+            />
+          ))}
+        </S.Platform>
+      </S.Content>
+      <S.Copyright>{`Copyright © ${date.getFullYear()} - Dwidasa Samsara Indonesia`}</S.Copyright>
+    </S.Wrapper>
+  )
+}
 
 export default Footer
