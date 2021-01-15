e9e2b35d54d9200f44ad3c7d4b1e68fc7c1626b3
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-15 19:10:03 -0300

    Config storybook of Button component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,32 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string',
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['normal', 'large'],
+      },
+    },
+    styles: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary'],
+      },
+    },
+  },
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Learn More',
+  size: 'normal',
+  styles: 'primary',
+}
