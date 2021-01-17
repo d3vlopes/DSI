e01ea7b800a2d08b764a61dbb4b19d3c7efecbf3
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-17 16:28:16 -0300

    Testing the Heading component

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -4,9 +4,33 @@ import { renderWithTheme } from 'utils/test/helpers'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render Heading', () => {
-    renderWithTheme(<Heading />)
+  it('should render the Heading with the grey color by default', () => {
+    renderWithTheme(<Heading>Children</Heading>)
 
-    expect(screen.getByRole('heading', { name: /Heading/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /children/i })).toHaveStyle({
+      color: '#1F2E35',
+    })
+  })
+
+  it('should render the wavy in red by default ', () => {
+    const { container } = renderWithTheme(<Heading wavy>Children</Heading>)
+
+    const wavy = container.querySelector('#wavy')
+    expect(wavy).toHaveStyle({
+      background: `url('/img/line-red.png') bottom repeat-x`,
+    })
+  })
+
+  it('should render the wavy in black color ', () => {
+    const { container } = renderWithTheme(
+      <Heading wavy wavyColor="littleBlack">
+        Children
+      </Heading>,
+    )
+
+    const wavy = container.querySelector('#wavy')
+    expect(wavy).toHaveStyle({
+      background: `url('/img/line-black.png') bottom repeat-x`,
+    })
   })
 })
