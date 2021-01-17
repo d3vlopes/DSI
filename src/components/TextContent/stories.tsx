e355bb958912e03fd60ee8274b99165e5267f309
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-17 17:20:10 -0300

    Config storybook of TextContent component

--- src/components/TextContent/stories.tsx
+++ src/components/TextContent/stories.tsx
@@ -1,9 +1,14 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import TextContent from '.'
+
+import TextContent, { TextContentProps } from '.'
+import mock from './mock'
 
 export default {
   title: 'TextContent',
-  component: TextContent
+  component: TextContent,
+  args: mock,
 } as Meta
 
-export const Default: Story = () => <TextContent />
+export const Default: Story<TextContentProps> = (args) => (
+  <TextContent {...args} />
+)
