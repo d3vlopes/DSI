80a4bdace56a4b0f8aabab157d585eec2e69eb20
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-17 17:19:32 -0300

    Creating structure of TextContent component

--- src/components/TextContent/index.tsx
+++ src/components/TextContent/index.tsx
@@ -1,8 +1,28 @@
+import Heading, { HeadingColor, HeadingWavyColor } from 'components/Heading'
+
 import * as S from './styles'
 
-const TextContent = () => (
-  <S.Wrapper>
-    <h1>TextContent</h1>
+export type TextContentProps = {
+  title?: string
+  titleColor?: HeadingColor
+  content: string
+  wavyColor?: HeadingWavyColor
+}
+
+const TextContent = ({
+  title,
+  titleColor = 'grey',
+  content,
+  wavyColor = 'primary',
+}: TextContentProps) => (
+  <S.Wrapper data-testid="wrapper">
+    {!!title && (
+      <Heading color={titleColor} wavy wavyColor={wavyColor}>
+        {title}
+      </Heading>
+    )}
+
+    <div dangerouslySetInnerHTML={{ __html: content }}></div>
   </S.Wrapper>
 )
 
