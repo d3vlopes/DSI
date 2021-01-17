17fb26ab33135e5bd26c78b1542716f2567eefe1
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-17 17:21:23 -0300

    Testing the TextContent component

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -3,10 +3,80 @@ import { renderWithTheme } from 'utils/test/helpers'
 
 import TextContent from '.'
 
-describe('<TextContent />', () => {
-  it('should render TextContent', () => {
-    renderWithTheme(<TextContent />)
+const props = {
+  title: 'Heading',
+  content: `
+<p>
+  A <a href="#">Link inside the text</a> with Lorem ipsum dolor sit,
+  amet consectetur adipisicing elit. Quia praesentium cupiditate tenetur
+  quam illum. Fugiat consectetur assumenda in, vitae nemo, eaque quia
+  numquam ad perspiciatis doloribus error eveniet quo officia?
+</p>`,
+}
 
-    expect(screen.getByRole('heading', { name: /TextContent/i })).toBeInTheDocument()
+describe('<TextContent />', () => {
+  it('should render the default(without title) component', () => {
+    renderWithTheme(<TextContent content={props.content} />)
+
+    expect(
+      screen.queryByRole('heading', { name: /heading/i }),
+    ).not.toBeInTheDocument()
+
+    expect(
+      screen.getByText(
+        /a with lorem ipsum dolor sit, amet consectetur adipisicing elit\. quia praesentium cupiditate tenetur quam illum\. fugiat consectetur assumenda in, vitae nemo, eaque quia numquam ad perspiciatis doloribus error eveniet quo officia\?/i,
+      ),
+    ).toBeInTheDocument()
+  })
+
+  it('should render title with gray color and content', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const heading = screen.getByRole('heading', { name: /heading/i })
+
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveStyle({
+      color: '#1F2E35',
+    })
+
+    expect(
+      screen.getByText(
+        /a with lorem ipsum dolor sit, amet consectetur adipisicing elit\. quia praesentium cupiditate tenetur quam illum\. fugiat consectetur assumenda in, vitae nemo, eaque quia numquam ad perspiciatis doloribus error eveniet quo officia\?/i,
+      ),
+    ).toBeInTheDocument()
+  })
+
+  it('should render the title with little black color and content', () => {
+    renderWithTheme(<TextContent {...props} titleColor="littleBlack" />)
+
+    const heading = screen.getByRole('heading', { name: /heading/i })
+
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveStyle({
+      color: '#263238',
+    })
+
+    expect(
+      screen.getByText(
+        /a with lorem ipsum dolor sit, amet consectetur adipisicing elit\. quia praesentium cupiditate tenetur quam illum\. fugiat consectetur assumenda in, vitae nemo, eaque quia numquam ad perspiciatis doloribus error eveniet quo officia\?/i,
+      ),
+    ).toBeInTheDocument()
+  })
+
+  it('should render the title with withe color and content', () => {
+    renderWithTheme(<TextContent {...props} titleColor="white" />)
+
+    const heading = screen.getByRole('heading', { name: /heading/i })
+
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveStyle({
+      color: '#FFFFFF',
+    })
+
+    expect(
+      screen.getByText(
+        /a with lorem ipsum dolor sit, amet consectetur adipisicing elit\. quia praesentium cupiditate tenetur quam illum\. fugiat consectetur assumenda in, vitae nemo, eaque quia numquam ad perspiciatis doloribus error eveniet quo officia\?/i,
+      ),
+    ).toBeInTheDocument()
   })
 })
