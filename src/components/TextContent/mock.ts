65d3fc787adaf41efa04d2532a513aea15056f1d
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-17 17:19:51 -0300

    Add mock for TextContent component

---
+++ src/components/TextContent/mock.ts
@@ -0,0 +1,61 @@
+import { TextContentProps } from '.'
+
+const TextContentMock: TextContentProps = {
+  title: 'Description',
+  content: `<h1>Heading</h1>
+<h2>Heading</h2>
+<h3>Heading</h3>
+<h4>Heading</h4>
+<h5>Heading</h5>
+<h6>Heading</h6>
+
+<img alt="" src="/img/item/pana.sbg" />
+
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+  tempore. Nulla accusamus tempora quaerat repellat accusantium
+  voluptatem reprehenderit iste dolorem quam rerum laboriosam architecto
+  eos, fugiat ab vero facilis soluta.
+</p>
+
+<p>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+  molestiae aut veniam fugiat dolor iste harum, quos cupiditate nihil
+  consequuntur eligendi similique nostrum aspernatur natus! Laboriosam
+  sint quis ad quibusdam?
+</p>
+
+<img alt="" src="/img/games/cyberpunk-2.jpg" />
+
+<ul>
+  <li>Lorem Ipsum</li>
+  <li>Lorem Ipsum</li>
+  <li>Lorem Ipsum</li>
+  <li>Lorem Ipsum</li>
+  <li>Lorem Ipsum</li>
+</ul>
+
+<hr />
+
+<ol>
+  <li>Lorem ipsum</li>
+  <li>Lorem ipsum</li>
+  <li>Lorem ipsum</li>
+  <li>Lorem ipsum</li>
+</ol>
+
+<p>
+  A <a href="#">Link inside the text</a> with Lorem ipsum dolor sit,
+  amet consectetur adipisicing elit. Quia praesentium cupiditate tenetur
+  quam illum. Fugiat consectetur assumenda in, vitae nemo, eaque quia
+  numquam ad perspiciatis doloribus error eveniet quo officia?
+</p>
+
+<h3>Welcome to Dwidasa Samsara Indonesia (DSI)</h3>
+<p>Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through DSI’s distinct front-end based application concept.
+
+Managed by a team of professional experts with extensive experience and impressive track records, DSI believes that continuous improvements and innovations assure your business to run effectively and efficiently.</p>
+`,
+}
+
+export default TextContentMock
