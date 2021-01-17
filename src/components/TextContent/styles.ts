1e4ce1ab5dbf84769626b996e53deb9b5c5389db
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-17 17:21:03 -0300

    Add style for TextContent component

--- src/components/TextContent/styles.ts
+++ src/components/TextContent/styles.ts
@@ -1,3 +1,42 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    ${media.greaterThan('medium')`
+      padding: ${theme.spacings.medium};
+    `}
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      margin: ${theme.spacings.xsmall} 0;
+    }
+
+    p {
+      margin-bottom: ${theme.spacings.xxlarge};
+    }
+
+    a {
+      color: ${theme.colors.primary};
+      text-decoration: none;
+    }
+
+    img {
+      max-width: min(70rem, 100%);
+      margin-bottom: ${theme.spacings.xsmall};
+    }
+
+    ul,
+    ol {
+      padding: ${theme.spacings.xsmall} ${theme.spacings.small};
+    }
+
+    hr {
+      margin: ${theme.spacings.small} 0;
+    }
+  `}
+`
