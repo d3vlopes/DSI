6a482522111b92015a3453672bfc3e12a9be6011
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-18 20:54:07 -0300

    Creating initial structure of Item component

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,8 +1,34 @@
+import TextContent from 'components/TextContent'
 import * as S from './styles'
 
-const Item = () => (
-  <S.Wrapper>
-    <h1>Item</h1>
+export type ItemProps = {
+  children?: React.ReactNode
+  color?: 'white' | 'whiteF6' | 'primary'
+  img?: string
+  alt?: string
+  reverse?: boolean
+  content?: string
+  title?: string
+}
+
+const Item = ({
+  children,
+  color = 'white',
+  img,
+  alt,
+  reverse = false,
+  content,
+  title,
+}: ItemProps) => (
+  <S.Wrapper img={img} reverse={reverse} color={color}>
+    {!!img && <img src={img} alt={alt} />}
+
+    {!!content && <TextContent content={content!} title={title!} />}
+    {!!children && (
+      <TextContent content={content!} title={title!}>
+        {children}
+      </TextContent>
+    )}
   </S.Wrapper>
 )
 
