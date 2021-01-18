1218f1a94a88bed8e435b42bfbce9d928958a6b5
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-18 20:54:29 -0300

    Add initial style for Item component

--- src/components/Item/styles.ts
+++ src/components/Item/styles.ts
@@ -1,3 +1,72 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+import { Wrapper as HeadingStyle } from 'components/Heading/styles'
+import { Wrapper as TextContentStyle } from 'components/TextContent/styles'
+
+import { ItemProps } from '.'
+
+export const Wrapper = styled.div<ItemProps>`
+  ${({ theme, color, reverse, img }) => css`
+    background: ${theme.colors[color!]};
+    padding: ${theme.spacings.medium};
+
+    > img {
+      width: 100%;
+      margin-bottom: ${theme.spacings.large};
+    }
+
+    ${HeadingStyle} {
+      margin-bottom: ${theme.spacings.huge};
+    }
+
+    h3,
+    h4,
+    h5,
+    h6 {
+      text-align: center;
+      max-width: 36.4rem;
+    }
+
+    p {
+      max-width: 35.8rem;
+      margin-top: ${theme.spacings.medium};
+    }
+
+    ${media.greaterThan('medium')`
+      display: flex;
+      flex-direction: ${reverse ? 'row-reverse' : 'flex'};
+      justify-content: ${img ? 'center' : 'flex-start'};
+
+      > img {
+        width: 50%;
+        max-height: 54.9rem;
+      }
+
+
+      p {
+        max-width: ${img ? '52rem' : '100%'};
+        text-align: inherit;
+      }
+
+      h3,
+      h4,
+      h5,
+      h6 {
+        text-align: inherit;
+        max-width: 100%;
+      }
+
+      ${TextContentStyle} {
+        margin-left: ${reverse ? '0' : '8.4rem'};
+        margin-right: ${reverse ? '8.4rem' : '0'};
+      }
+    `}
+
+    ${media.greaterThan('large')`
+      > img {
+        width: auto;
+      }
+    `}
+  `}
+`
