2eca48b645eadd802388301ce11de28bd7ba369b
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-18 20:53:23 -0300

    Add mock for Item component

---
+++ src/components/Item/mock.ts
@@ -0,0 +1,36 @@
+export default [
+  {
+    img: '/img/item/home.png',
+    alt: 'person working on the computer',
+    content: `<h3>Welcome to Dwidasa Samsara Indonesia (DSI)</h3>
+    <p>Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through DSI’s distinct front-end based application concept.</p>
+    <p>Managed by a team of professional experts with extensive experience and impressive track records, DSI believes that continuous improvements and innovations assure your business to run effectively and efficiently.</p>`,
+  },
+  {
+    title: 'Our Technology',
+    content: `<p>First JAVA, runs on more than 850 million personal computers worldwide, and on billions of devices worldwide, including mobile and TV devices.The latest Java version contains important enhancements to improve performance, stability and security of the Java applications that run on your machine. Installing this free update will ensure that your Java applications continue to run safely and efficiently. The best part of JAVA is free.</p>
+
+    <p>We also use PHP for general occation, why? we think you already know the reason,of course because is the most flexible language on earth that can colaborate very well with all language.</p>
+
+    <p>for the strusture is HTML & CSS ( who does not know ? ) cause it's the basic language use for the web and help us creating a very outstanding view. its become important cause it make our system looks good in front of the eyes of our client, (doesnt agree?), Hmm, let me ask you if you have a good software but with bad UI/UX, do people use it? cause for it we can reach our goals.</p>
+
+    <p>What about Microsoft technology, why not. We always strive to remain the best and try to keep developing what we have.</p>`,
+  },
+  {
+    title: 'Our Service',
+    img: '/img/item/chat.png',
+    alt: 'people with a cell phone in the background with an open chat',
+    content: `<p>DSI’s shared service solutions focus on the front-end based software development, designed specifically for the banking and financial sectors. We short-cut your front-liners to customers interaction process through a way that mediates your business process in a more direct communication means, in just a smile away from winning your customer’s trust.</p>
+
+    <p>Through close partnership, we always work together with your team, all throughout from pretopost project cycles – initial planning, project development, implementation, evaluation up to reporting documentation – to fully coach you so that all your specific business.</p>
+
+    <p>requirements can be delivered at the right time to the right audience of particular interests. Moreover, our team will support you to provide a detailed consultation throughout operation stage, and after the project implementation cycles by applying specific monitoring and improvement methodology. Our experienced people will drive your personalized service, by utilizing the best technology supports no other businesses can provide.</p>
+
+    `,
+  },
+  {
+    title: 'Our Future Prespective',
+    img: '/img/item/thumbnail-1.png',
+    alt: 'thumbnail video',
+  },
+]
