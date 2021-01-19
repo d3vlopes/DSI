691d1325493eb870cb76baa23b6f46726a137444
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-19 20:43:30 -0300

    Add mock for Icon component

---
+++ src/components/Icon/mock.ts
@@ -0,0 +1,25 @@
+export default [
+  {
+    title: 'Vision',
+    content:
+      '<p>To become the leading application service provider (ASP) focusing on front-end application delivered for Indonesia’s banking and fi nancial industry.</p>',
+  },
+  {
+    title: 'Mission',
+    content:
+      '<p>To grow the business together with clients by providing enhanced services and wide delivery channel to end-users.</p>',
+  },
+  {
+    title: 'Address',
+    content:
+      '<span>Jl. Halim Perdana Kusuma, Ring Road Bangkalan, Madura, Jawa Timur.</span>',
+  },
+  {
+    title: 'Email',
+    content: '<span>sekolahku@sch.id</span>',
+  },
+  {
+    title: 'Telephone',
+    content: '<span>(031) 3092223</span>',
+  },
+]
