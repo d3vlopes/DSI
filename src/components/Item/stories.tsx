ef540b310f25bd077db9c1db08fa248da0979527
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-01-21 17:04:45 -0300

    Config WithChildren on storybook of Item component

--- src/components/Item/stories.tsx
+++ src/components/Item/stories.tsx
@@ -1,5 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { UserGroup } from '@styled-icons/heroicons-outline/UserGroup'
+import { Dashboard3 } from '@styled-icons/remix-line/Dashboard3'
+
+import Icon from 'components/Icon'
 import Item, { ItemProps } from '.'
+
 import mock from './mock'
 
 export default {
@@ -8,6 +13,14 @@ export default {
   args: mock[0],
   parameters: {
     layout: 'fullscreen',
+  },
+  argTypes: {
+    children: {
+      type: '',
+    },
+    content: {
+      type: '',
+    },
   },
 } as Meta
 
@@ -50,6 +63,19 @@ export const WithChildren: Story<ItemProps> = (args) => (
 WithChildren.args = {
   img: mock[3].img,
   title: mock[3].title,
-  content: '',
-  children: 'Content',
+  content: mock[3].content,
+  children: (
+    <div>
+      <Icon
+        icon={<UserGroup />}
+        title="Vision"
+        content="<p>To become the leading application service provider (ASP) focusing on front-end application delivered for Indonesia’s banking and fi nancial industry.</p>"
+      />
+      <Icon
+        icon={<Dashboard3 />}
+        title="Mission"
+        content="<p>To grow the business together with clients by providing enhanced services and wide delivery channel to end-users.</p>"
+      />
+    </div>
+  ),
 }
