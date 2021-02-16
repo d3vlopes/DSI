4058fac285c3347f1f87a4371dd8b91be5c1b30f
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-16 15:01:29 -0300

    Add mock for Provides component

---
+++ src/components/Provides/mock.ts
@@ -0,0 +1,24 @@
+import { ProvideProps } from '.'
+
+const ProvidesMock: ProvideProps[] = [
+  {
+    image: 'img/products/product.png',
+    title: 'Our Product',
+    description:
+      'Our product is made on the base of our team’s careful research and analyses, ranging from internet based application.',
+  },
+  {
+    image: 'img/products/service.png',
+    title: 'Our Service',
+    description:
+      'DSI’s shared service solutions focus on the front-end based software development, designed specifically for the banking and financial sectors.',
+  },
+  {
+    image: 'img/products/technology.png',
+    title: 'Our Technology',
+    description:
+      'First JAVA, runs on more than 850 million personal computers worldwide, and on billions of devices worldwide, including mobile and TV devices.',
+  },
+]
+
+export default ProvidesMock
