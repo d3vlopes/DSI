7a89d49d73d8c62c29f61a210f10b2966e6a0d2b
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-17 20:50:56 -0300

    Add styles for Hero component

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,3 +1,23 @@
-import styled from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
+import { ColorBorder, HeroProps } from '.'
 
-export const Wrapper = styled.main``
+const wrapperModifiers = {
+  bottom: (theme: DefaultTheme, colorBorder: ColorBorder) => css`
+    background: ${theme.colors[colorBorder]};
+    height: ${colorBorder === 'primary' ? '1.7rem' : '3.6rem'};
+  `,
+}
+
+export const Wrapper = styled.div<HeroProps>`
+  ${({ theme, backgroundColor }) => css`
+    background: ${theme.colors[backgroundColor!]};
+  `}
+`
+
+export const Bottom = styled.div<
+  Pick<HeroProps, 'colorBorder' | 'borderStyle'>
+>`
+  ${({ theme, borderStyle, colorBorder }) => css`
+    ${!!borderStyle && wrapperModifiers[borderStyle!](theme, colorBorder!)}
+  `}
+`
