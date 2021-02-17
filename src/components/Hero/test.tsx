8b126cf32cac66c2dbe0ae6ae06463429655e278
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-17 20:51:11 -0300

    Testing the Hero component

--- src/components/Hero/test.tsx
+++ src/components/Hero/test.tsx
@@ -4,9 +4,37 @@ import { renderWithTheme } from 'utils/test/helpers'
 import Hero from '.'
 
 describe('<Hero />', () => {
-  it('should render Hero', () => {
-    renderWithTheme(<Hero />)
+  it('should render the default Hero', () => {
+    const { container } = renderWithTheme(<Hero>content</Hero>)
 
-    expect(screen.getByRole('heading', { name: /Hero/i })).toBeInTheDocument()
+    expect(screen.getByText(/content/i)).toBeInTheDocument()
+
+    expect(container.querySelector('#hero')).toHaveStyle({
+      background: '#FFFFFF',
+    })
+  })
+
+  it('should render the Hero with border bottom on color whiteF6 by default', () => {
+    const { container } = renderWithTheme(
+      <Hero borderStyle="bottom">content</Hero>,
+    )
+
+    expect(container.querySelector('#hero > div')).toHaveStyle({
+      background: '#F6F6F6',
+      height: '3.6rem',
+    })
+  })
+
+  it('should render the Hero with border bottom on color primay', () => {
+    const { container } = renderWithTheme(
+      <Hero borderStyle="bottom" colorBorder="primary">
+        content
+      </Hero>,
+    )
+
+    expect(container.querySelector('#hero > div')).toHaveStyle({
+      background: '#EE4248',
+      height: '1.7rem',
+    })
   })
 })
