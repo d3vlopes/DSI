5908d9dc194407154dafd59508b88dd7b17b67bf
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-19 20:55:29 -0300

    Testing correctly render of hero on Home template

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -3,10 +3,29 @@ import { renderWithTheme } from 'utils/test/helpers'
 
 import Home from '.'
 
+import mockHero from 'components/Hero/mock'
+
+const props = {
+  hero: mockHero[0],
+}
+
+jest.mock('components/Hero', () => {
+  return {
+    __esModule: true,
+    default: function Mock({ children }: { children: React.ReactNode }) {
+      return (
+        <div id="hero" data-testid="Mock Hero">
+          {children}
+        </div>
+      )
+    },
+  }
+})
+
 describe('<Home />', () => {
   it('should render Home', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Hero')).toBeInTheDocument()
   })
 })
