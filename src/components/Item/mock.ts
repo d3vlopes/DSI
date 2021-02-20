8e66cabaeafe1d2ab8f075cdd47b06d2ed5b4766
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-02-20 00:22:43 -0300

    Add section about on Home template

--- src/components/Item/mock.ts
+++ src/components/Item/mock.ts
@@ -2,9 +2,9 @@ export default [
   {
     img: '/img/item/home.png',
     alt: 'person working on the computer',
-    content: `<h3>Welcome to Dwidasa Samsara Indonesia (DSI)</h3>
-    <p>Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through DSI’s distinct front-end based application concept.</p>
-    <p>Managed by a team of professional experts with extensive experience and impressive track records, DSI believes that continuous improvements and innovations assure your business to run effectively and efficiently.</p>`,
+    content: `<h3>Welcome to <span>Dwidasa Samsara Indonesia (DSI)<span></h3>
+    <p>Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through <span>DSI</span>’s distinct front-end based application concept.</p>
+    <p>Managed by a team of professional experts with extensive experience and impressive track records, <span>DSI</span> believes that continuous improvements and innovations assure your business to run effectively and efficiently.</p>`,
   },
   {
     title: 'Our Technology',
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -3,6 +3,7 @@ import Base from 'templates/Base'
 import Hero from 'components/Hero'
 import Button from 'components/Button'
 import { Container } from 'components/Container'
+import Item, { ItemProps } from 'components/Item'
 
 import * as S from './styles'
 
@@ -12,37 +13,42 @@ export type HomeTemplateProps = {
     description: string
     src: string
   }
+  about: ItemProps
 }
 
-const Home = ({ hero }: HomeTemplateProps) => (
+const Home = ({ hero, about }: HomeTemplateProps) => (
   <S.Wrapper>
     <Base>
       <S.SectionHero>
-        <Hero>
-          <Container>
+        <Container>
+          <Hero>
             <S.HeroContent>
-              <div>
-                <S.HeroTitle>
-                  {hero.title.slice(0, 36)}
-                  <br />
-                  <span>{hero.title.slice(36, 58)}</span>
-                </S.HeroTitle>
-              </div>
-
+              <S.HeroTitle>
+                {hero.title.slice(0, 36)}
+                <br />
+                <span>{hero.title.slice(36, 58)}</span>
+              </S.HeroTitle>
               <S.HeroDescription>{hero.description}</S.HeroDescription>
-
               <Button>Read More</Button>
             </S.HeroContent>
-
             <S.HeroImage>
               <img
                 src={hero.src}
                 alt="A person next to various types of devices"
               />
             </S.HeroImage>
-          </Container>
-        </Hero>
+          </Hero>
+        </Container>
       </S.SectionHero>
+
+      <S.SectionAbout>
+        <Item
+          img={about.img}
+          alt={about.alt}
+          content={about.content}
+          color="whiteF6"
+        />
+      </S.SectionAbout>
     </Base>
   </S.Wrapper>
 )
