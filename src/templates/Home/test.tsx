6e7ef58237f72534fff08a8f22fc6fc9f1ec9803
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-01 17:12:08 -0300

    Testing correct rendering of the about section in the Home template

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -4,9 +4,11 @@ import { renderWithTheme } from 'utils/test/helpers'
 import Home from '.'
 
 import mockHero from 'components/Hero/mock'
+import mockAbout from 'components/Item/mock'
 
 const props = {
   hero: mockHero[0],
+  about: mockAbout[0],
 }
 
 jest.mock('components/Hero', () => {
@@ -22,10 +24,20 @@ jest.mock('components/Hero', () => {
   }
 })
 
+jest.mock('components/Item', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock About"></div>
+    },
+  }
+})
+
 describe('<Home />', () => {
   it('should render Home', () => {
     renderWithTheme(<Home {...props} />)
 
     expect(screen.getByTestId('Mock Hero')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock About')).toBeInTheDocument()
   })
 })
