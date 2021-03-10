7795f2f858f13a78fcce4195f2b53ab43bba7c80
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-10 17:14:37 -0300

    Passing data of provides on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Home, { HomeTemplateProps } from 'templates/Home'
 
 import mockHero from 'components/Hero/mock'
 import mockAbout from 'components/Item/mock'
+import mockProvides from 'components/Provides/mock'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -12,6 +13,7 @@ export async function getStaticProps() {
     props: {
       hero: mockHero[0],
       about: mockAbout[0],
+      provides: mockProvides,
     },
   }
 }
