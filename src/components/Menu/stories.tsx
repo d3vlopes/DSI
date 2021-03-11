25f2afeaeeb61b87c2c6d8030e3cbced7f4187c8
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-11 16:50:47 -0300

    Add Active Link on storybook of Menu component

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,13 +1,31 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Menu from '.'
+import Menu, { MenuProps } from '.'
 
 export default {
   title: 'Menu',
   component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  argTypes: {
+    activeLink: {
+      control: {
+        type: 'select',
+        options: [
+          '/services',
+          '/product',
+          '/technology',
+          '/about',
+          '/client',
+          '/partner',
+        ],
+      },
+    },
+  },
 } as Meta
 
-export const Default: Story = () => <Menu />
+export const Default: Story<MenuProps> = (args) => <Menu {...args} />
 
-Default.parameters = {
-  layout: 'fullscreen',
-}
+export const ActiveLink: Story<MenuProps> = (args) => (
+  <Menu activeLink="/services" {...args} />
+)
