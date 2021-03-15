87db09126f364b7840140f5377b47366e6ff6fc1
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-15 20:53:24 -0300

    Creating type for content of Hero component in mock

--- src/components/Hero/mock.ts
+++ src/components/Hero/mock.ts
@@ -1,8 +1,19 @@
-export default [
+export type HeroContent = {
+  title: string
+  description?: string
+  src?: string
+}
+
+export const HeroMock: HeroContent[] = [
   {
     title: 'Making the most of the ever-growing Information Technology',
     description:
       'Managed by a team of professional experts with extensive experience and impressive track records',
     src: 'img/hero/home.png',
   },
+  {
+    title: 'Just a smile away from Winning your customer’s trust',
+  },
 ]
+
+export default HeroMock
