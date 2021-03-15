3afc7a228141eb23a118d2bda1f5c207e37dea87
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-15 20:54:37 -0300

    Add type HeroContent on prop hero of Home template

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -6,61 +6,56 @@ import Heading from 'components/Heading'
 import Item, { ItemProps } from 'components/Item'
 import Provides, { ProvideProps } from 'components/Provides'
 import { Container } from 'components/Container'
+import { HeroContent } from 'components/Hero/mock'
 
 import * as S from './styles'
 
 export type HomeTemplateProps = {
-  hero: {
-    title: string
-    description: string
-    src: string
-  }
+  hero: HeroContent
   about: ItemProps
   provides: ProvideProps[]
 }
 
 const Home = ({ hero, about, provides }: HomeTemplateProps) => (
-  <S.Wrapper>
-    <Base>
-      <S.SectionHero>
-        <Container>
-          <Hero>
-            <S.HeroContent>
-              <S.HeroTitle>
-                {hero.title.slice(0, 36)}
-                <br />
-                <span>{hero.title.slice(36, 58)}</span>
-              </S.HeroTitle>
-              <S.HeroDescription>{hero.description}</S.HeroDescription>
-              <Button>Read More</Button>
-            </S.HeroContent>
-            <S.HeroImage>
-              <img
-                src={hero.src}
-                alt="A person next to various types of devices"
-              />
-            </S.HeroImage>
-          </Hero>
-        </Container>
-      </S.SectionHero>
+  <Base>
+    <S.SectionHero>
+      <Container>
+        <Hero>
+          <S.HeroContent>
+            <S.HeroTitle>
+              {hero.title.slice(0, 36)}
+              <br />
+              <span>{hero.title.slice(36, 58)}</span>
+            </S.HeroTitle>
+            <S.HeroDescription>{hero.description}</S.HeroDescription>
+            <Button>Read More</Button>
+          </S.HeroContent>
+          <S.HeroImage>
+            <img
+              src={hero.src}
+              alt="A person next to various types of devices"
+            />
+          </S.HeroImage>
+        </Hero>
+      </Container>
+    </S.SectionHero>
 
-      <S.SectionAbout>
-        <Item
-          img={about.img}
-          alt={about.alt}
-          content={about.content}
-          color="whiteF6"
-        />
-      </S.SectionAbout>
+    <S.SectionAbout>
+      <Item
+        img={about.img}
+        alt={about.alt}
+        content={about.content}
+        color="whiteF6"
+      />
+    </S.SectionAbout>
 
-      <S.SectionProvides>
-        <Heading>Product and Service</Heading>
-        <Container>
-          <Provides items={provides} />
-        </Container>
-      </S.SectionProvides>
-    </Base>
-  </S.Wrapper>
+    <S.SectionProvides>
+      <Heading>Product and Service</Heading>
+      <Container>
+        <Provides items={provides} />
+      </Container>
+    </S.SectionProvides>
+  </Base>
 )
 
 export default Home
