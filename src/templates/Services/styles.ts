93cbe542539bba44662861f50f06cccfb89ad1eb
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-15 21:07:45 -0300

    Add styles for hero section on Services template

---
+++ src/templates/Services/styles.ts
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const HeroContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5rem 0 8.8rem;
+
+  ${media.greaterThan('medium')`
+    padding: 7.4rem 0 3.2rem;
+  `}
+`
+
+export const HeroTitle = styled.h2`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xlarge};
+    font-weight: ${theme.font.weight.regular};
+    line-height: ${theme.font.sizes.xxlarge};
+    color: ${theme.colors.littleBlack};
+    text-align: center;
+    max-width: 36.1rem;
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+      line-height: ${theme.font.sizes.xxxlarge};
+      max-width: 54.5rem;
+    `}
+
+    > span {
+      color: ${theme.colors.primary};
+      font-weight: ${theme.font.weight.bold};
+    }
+  `}
+`
