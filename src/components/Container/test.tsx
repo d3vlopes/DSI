cf31c21cdc17af6ca141f3d7a38ad2c87e76d7fa
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-03-17 16:05:11 -0300

    FIX Container component styles

--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -22,8 +22,8 @@ describe('<Container />', () => {
         max-width: 130rem;
         margin-left: auto;
         margin-right: auto;
-        padding-left: calc(3.2rem / 2);
-        padding-right: calc(3.2rem / 2);
+        padding-left: 3.2rem;
+        padding-right: 3.2rem;
       }
 
       <div
