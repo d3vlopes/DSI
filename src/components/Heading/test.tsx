8f1eda3a1af7bbef0b2199c73028e0efdcda07c0
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-04-26 20:49:34 -0300

    FIX test default  of Heading component

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -4,21 +4,16 @@ import { ItemDefaultValues } from 'hooks/use-item'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render the Heading with the grey color by default', () => {
-    render(<Heading>Children</Heading>)
+  it('should render by default Heading with tag h2 and color grey', () => {
+    const { container } = render(<Heading>Children</Heading>)
+
+    const h2 = container.querySelector('div > h2')
 
     expect(screen.getByRole('heading', { name: /children/i })).toHaveStyle({
       color: '#1F2E35',
     })
-  })
 
-  it('should render the wavy in red by default ', () => {
-    const { container } = render(<Heading wavy>Children</Heading>)
-
-    const wavy = container.querySelector('#wavy')
-    expect(wavy).toHaveStyle({
-      background: `url('/img/line-red.png') bottom repeat-x`,
-    })
+    expect(h2).toBeInTheDocument()
   })
 
   it('should Heading must be rendered with the h1 tag and with wavy', () => {
@@ -34,6 +29,15 @@ describe('<Heading />', () => {
     const h1 = container.querySelector('div > h1')
 
     expect(h1).toBeInTheDocument()
+
+    const wavy = container.querySelector('#wavy')
+    expect(wavy).toHaveStyle({
+      background: `url('/img/line-red.png') bottom repeat-x`,
+    })
+  })
+
+  it('should render the wavy in red by default ', () => {
+    const { container } = render(<Heading wavy>Children</Heading>)
 
     const wavy = container.querySelector('#wavy')
     expect(wavy).toHaveStyle({
